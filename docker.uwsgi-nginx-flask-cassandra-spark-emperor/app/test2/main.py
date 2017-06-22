ec22980778fb928c107a347f8f99e1c0aaadaba4
Jennifer <cutejaneii@hotmail.com>
2017-06-22 14:30:40 +0800

    Create main.py

---
+++ docker.uwsgi-nginx-flask-cassandra-spark-emperor/app/test2/main.py
@@ -0,0 +1,17 @@
+# encoding=UTF-8
+#!flask/bin/python
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+        return "Hello, this is test 2 ~"
+
+@app.route('/index', methods=['GET'])
+def index2():
+        return "Hello, this is test 2 index~"
+
+
+if __name__ ==  '__main__':
+        app.run(debug=True)
